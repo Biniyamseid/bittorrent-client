a9a1e2b9a81129fb6320fad45b018514da0c3541
Biniyamseid <Biniyamseid3@gmail.com>
2024-02-01 05:16:05 +0200

    forth stage

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -1,149 +1,131 @@
 package main
 
 import (
-	// Uncomment this line to pass the first stage
-	// "encoding/json"
 	"encoding/json"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
-	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
-// Example:
-// - 5:hello -> hello
-// - 10:hello12345 -> hello12345
-// func decodeBencode(bencodedString string) (interface{}, error) {
+func decodeBencode(bencodedString string) (interface{}, int, error) {
+	if unicode.IsDigit(rune(bencodedString[0])) {
+		// string case
+		var firstColonIndex int
 
-// 	if strings.HasPrefix(bencodedString, "i") && strings.HasSuffix(bencodedString, "e") {
-// 		return strconv.Atoi(bencodedString[1 : len(bencodedString)-1])
-// 	}
-// 	if strings.HasPrefix(bencodedString, "l") && strings.HasSuffix(bencodedString, "e") {
-// 		var decodedList []interface{}
-// 		for _, element := range strings.Split(bencodedString[1:len(bencodedString)-1], "e") {
-// 			decodedElement, err := decodeBencode(element + "e")
-// 			if err != nil {
-// 				return nil, err
-// 			}
-// 			decodedList = append(decodedList, decodedElement)
-// 		}
-// 		return decodedList, nil
-// 	}
-// 	if unicode.IsDigit(rune(bencodedString[0])) {
-// 		var firstColonIndex int
-
-// 		for i := 0; i < len(bencodedString); i++ {
-// 			if bencodedString[i] == ':' {
-// 				firstColonIndex = i
-// 				break
-// 			}
-// 		}
-
-// 		lengthStr := bencodedString[:firstColonIndex]
-
-// 		length, err := strconv.Atoi(lengthStr)
-// 		if err != nil {
-// 			return "", err
-// 		}
-
-// 		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-// 	} else {
-// 		return "", fmt.Errorf("Only strings are supported at the moment")
-// 	}
-
-// }
-
-func DecodeString(bencodedString string) (string, error) {
-	var firstColonIndex int
-	for i := 0; i < len(bencodedString); i++ {
-		if bencodedString[i] == ':' {
-			firstColonIndex = i
-			break
+		for i := 0; i < len(bencodedString); i++ {
+			if bencodedString[i] == ':' {
+				firstColonIndex = i
+				break
+			}
 		}
-	}
-	lengthStr := bencodedString[:firstColonIndex]
-	length, err := strconv.Atoi(lengthStr)
-	if err != nil {
-		return "", err
-	}
-	return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-}
-func DecodeInteger(bencodedString string) (int, error) {
-	var endAt int
-	for i := 0; i < len(bencodedString); i++ {
-		if bencodedString[i] == 'e' {
-			endAt = i
-			break
-		}
-	}
-	stringToDecode := bencodedString[1:endAt]
-	return strconv.Atoi(stringToDecode)
-}
-func DecodeList(bencodedString string) ([]interface{}, error) {
-	values := []interface{}{}
-	if len(bencodedString) < 2 {
-		return values, fmt.Errorf("bencoded lists are at least two characters long")
-	}
-	if bencodedString[0] != 'l' {
-		return values, fmt.Errorf("bencoded lists start with l")
-	}
-	if len(bencodedString) == 2 {
-		return values, nil
-	}
-	position := 0
-	bencodedList := bencodedString[1 : len(bencodedString)-1]
-	for position < len(bencodedList)-1 {
-		listToDecode := bencodedList[position:]
-		element, err := decodeBencode(listToDecode)
+
+		lengthStr := bencodedString[:firstColonIndex]
+
+		length, err := strconv.Atoi(lengthStr)
 		if err != nil {
-			return []interface{}{}, err
+			return "", 0, err
 		}
-		values = append(values, element)
-		position += len(fmt.Sprint(element)) + 2
-		if position >= len(bencodedList) || bencodedList[position] == 'e' {
-			break
-		}
-	}
-	return values, nil
-}
-func decodeBencode(bencodedString string) (interface{}, error) {
-	if len(bencodedString) == 0 {
-		return "", fmt.Errorf("empty string provided")
-	}
-	firstChar := rune(bencodedString[0])
-	switch {
-	case unicode.IsDigit(firstChar):
-		return DecodeString(bencodedString)
-	case firstChar == 'i':
-		return DecodeInteger(bencodedString)
-	case firstChar == 'l':
-		return DecodeList(bencodedString)
-	default:
-		return nil, fmt.Errorf("unexpected rune")
-	}
-}
 
+		untilIndex := firstColonIndex + 1 + length
+		return bencodedString[firstColonIndex+1 : untilIndex], untilIndex, nil
+	} else if strings.HasPrefix(bencodedString, "i") {
+		// integers case
+		var endIndex int
+
+		for i := 0; i < len(bencodedString); i++ {
+			if bencodedString[i] == 'e' {
+				endIndex = i
+				break
+			}
+		}
+
+		num, err := strconv.Atoi(bencodedString[1:endIndex])
+		if err != nil {
+			return "", 0, err
+		}
+
+		return num, endIndex + 1, nil
+	} else if strings.HasPrefix(bencodedString, "l") {
+		// list case
+		in := strings.TrimPrefix(bencodedString, "l")
+
+		var (
+			ret        = []interface{}{}
+			untilIndex int
+		)
+		for {
+			if in[0] == 'e' {
+				break
+			}
+
+			decoded, nextIndex, err := decodeBencode(in)
+			if err != nil {
+				return "", 0, err
+			}
+			ret = append(ret, decoded)
+
+			in = in[nextIndex:]
+			untilIndex += nextIndex
+		}
+
+		return ret, untilIndex + 1, nil
+	} else if strings.HasPrefix(bencodedString, "d") {
+		// dictionary case
+		in := strings.TrimPrefix(bencodedString, "d")
+
+		var (
+			ret        = map[string]interface{}{}
+			key        string
+			untilIndex int
+		)
+		for {
+			if in[0] == 'e' {
+				break
+			}
+
+			decoded, nextIndex, err := decodeBencode(in)
+			if err != nil {
+				return "", 0, err
+			}
+			if key == "" {
+				key = decoded.(string)
+			} else {
+				ret[key] = decoded
+				key = ""
+			}
+
+			in = in[nextIndex:]
+			untilIndex += nextIndex
+		}
+
+		return ret, untilIndex + 1, nil
+	} else {
+		return "", 0, fmt.Errorf("unexpected format")
+	}
+}
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	// fmt.Println("Logs from your program will appear here!")
-
 	command := os.Args[1]
 
-	if command == "decode" {
-		// Uncomment this block to pass the first stage
-
+	switch command {
+	case "decode":
 		bencodedValue := os.Args[2]
 
-		decoded, err := decodeBencode(bencodedValue)
+		decoded, _, err := decodeBencode(bencodedValue)
 		if err != nil {
+
 			fmt.Println(err)
 			return
 		}
+
 		jsonOutput, _ := json.Marshal(decoded)
 		fmt.Println(string(jsonOutput))
-	} else {
+
+	default:
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
+
 	}
+
 }
