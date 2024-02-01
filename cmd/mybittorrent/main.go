6396ea5b1bfa7f9c6aec13310e831171442e67d5
Biniyamseid <Biniyamseid3@gmail.com>
2024-02-01 04:35:27 +0200

    pass 3 stage

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 	"unicode"
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
@@ -15,33 +14,114 @@ import (
 // Example:
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
-func decodeBencode(bencodedString string) (interface{}, error) {
+// func decodeBencode(bencodedString string) (interface{}, error) {
 
-	if strings.HasPrefix(bencodedString, "i") && strings.HasSuffix(bencodedString, "e") {
-		return strconv.Atoi(bencodedString[1 : len(bencodedString)-1])
-	}
-	if unicode.IsDigit(rune(bencodedString[0])) {
-		var firstColonIndex int
+// 	if strings.HasPrefix(bencodedString, "i") && strings.HasSuffix(bencodedString, "e") {
+// 		return strconv.Atoi(bencodedString[1 : len(bencodedString)-1])
+// 	}
+// 	if strings.HasPrefix(bencodedString, "l") && strings.HasSuffix(bencodedString, "e") {
+// 		var decodedList []interface{}
+// 		for _, element := range strings.Split(bencodedString[1:len(bencodedString)-1], "e") {
+// 			decodedElement, err := decodeBencode(element + "e")
+// 			if err != nil {
+// 				return nil, err
+// 			}
+// 			decodedList = append(decodedList, decodedElement)
+// 		}
+// 		return decodedList, nil
+// 	}
+// 	if unicode.IsDigit(rune(bencodedString[0])) {
+// 		var firstColonIndex int
 
-		for i := 0; i < len(bencodedString); i++ {
-			if bencodedString[i] == ':' {
-				firstColonIndex = i
-				break
-			}
+// 		for i := 0; i < len(bencodedString); i++ {
+// 			if bencodedString[i] == ':' {
+// 				firstColonIndex = i
+// 				break
+// 			}
+// 		}
+
+// 		lengthStr := bencodedString[:firstColonIndex]
+
+// 		length, err := strconv.Atoi(lengthStr)
+// 		if err != nil {
+// 			return "", err
+// 		}
+
+// 		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
+// 	} else {
+// 		return "", fmt.Errorf("Only strings are supported at the moment")
+// 	}
+
+// }
+
+func DecodeString(bencodedString string) (string, error) {
+	var firstColonIndex int
+	for i := 0; i < len(bencodedString); i++ {
+		if bencodedString[i] == ':' {
+			firstColonIndex = i
+			break
 		}
-
-		lengthStr := bencodedString[:firstColonIndex]
-
-		length, err := strconv.Atoi(lengthStr)
+	}
+	lengthStr := bencodedString[:firstColonIndex]
+	length, err := strconv.Atoi(lengthStr)
+	if err != nil {
+		return "", err
+	}
+	return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
+}
+func DecodeInteger(bencodedString string) (int, error) {
+	var endAt int
+	for i := 0; i < len(bencodedString); i++ {
+		if bencodedString[i] == 'e' {
+			endAt = i
+			break
+		}
+	}
+	stringToDecode := bencodedString[1:endAt]
+	return strconv.Atoi(stringToDecode)
+}
+func DecodeList(bencodedString string) ([]interface{}, error) {
+	values := []interface{}{}
+	if len(bencodedString) < 2 {
+		return values, fmt.Errorf("bencoded lists are at least two characters long")
+	}
+	if bencodedString[0] != 'l' {
+		return values, fmt.Errorf("bencoded lists start with l")
+	}
+	if len(bencodedString) == 2 {
+		return values, nil
+	}
+	position := 0
+	bencodedList := bencodedString[1 : len(bencodedString)-1]
+	for position < len(bencodedList)-1 {
+		listToDecode := bencodedList[position:]
+		element, err := decodeBencode(listToDecode)
 		if err != nil {
-			return "", err
+			return []interface{}{}, err
 		}
-
-		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-	} else {
-		return "", fmt.Errorf("Only strings are supported at the moment")
+		values = append(values, element)
+		position += len(fmt.Sprint(element)) + 2
+		if position >= len(bencodedList) || bencodedList[position] == 'e' {
+			break
+		}
 	}
-
+	return values, nil
+}
+func decodeBencode(bencodedString string) (interface{}, error) {
+	if len(bencodedString) == 0 {
+		return "", fmt.Errorf("empty string provided")
+	}
+	firstChar := rune(bencodedString[0])
+	switch {
+	case unicode.IsDigit(firstChar):
+		return DecodeString(bencodedString)
+	case firstChar == 'i':
+		return DecodeInteger(bencodedString)
+	case firstChar == 'l':
+		return DecodeList(bencodedString)
+	default:
+		return nil, fmt.Errorf("unexpected rune")
+	}
 }
 
 func main() {
